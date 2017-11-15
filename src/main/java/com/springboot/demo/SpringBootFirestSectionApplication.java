eb8a32a1f10fe8d6224b024ba2286e7df1cc653a
聂子 <1021491936@qq.com>
2017-11-15 11:49:21 +0800

    创建第一节工程

---
+++ src/main/java/com/springboot/demo/SpringBootFirestSectionApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirestSectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirestSectionApplication.class, args);
+	}
+}
